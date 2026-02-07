e01a6ea740046ffcac2db6886a04f2a6108e344d
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-08 00:41:12 +0500

    feat: add navbar with logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  );
-};
+    <div className='flex items-center gap-16 bg-gray-700 py-3 px-8'>
+       <img className='w-16' src="https://png.pngtree.com/png-vector/20220930/ourmid/pngtree-shopping-logo-design-for-online-store-website-png-image_6239056.png" alt="" />
+    <div className='flex gap-7 text-2xl'>
+       <Link to='/'>Home</Link>
+        <Link to='/product'>Product</Link>
+    </div>
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
