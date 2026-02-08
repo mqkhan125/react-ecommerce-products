7a35eb8b895832a49dc61045ec685660e14275fa
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-08 16:33:05 +0500

    feat: enhance navbar with logo navigation and active link styles

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div className='flex items-center gap-16 bg-gray-700 py-3 px-8'>
-       <img className='w-16' src="https://png.pngtree.com/png-vector/20220930/ourmid/pngtree-shopping-logo-design-for-online-store-website-png-image_6239056.png" alt="" />
-    <div className='flex gap-7 text-2xl'>
-       <Link to='/'>Home</Link>
-        <Link to='/product'>Product</Link>
-    </div>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Navbar
+  return (
+    <div className="flex items-center gap-16 bg-gray-700 py-3 px-8">
+      <img
+        className="w-16"
+        src="https://png.pngtree.com/png-vector/20220930/ourmid/pngtree-shopping-logo-design-for-online-store-website-png-image_6239056.png"
+        alt="logo"
+        onClick={() => navigate("/")}
+      />
+      <div className="flex gap-7 text-2xl text-white font-semibold">
+         <Menu to={'/'} title={'Home'}/>
+         <Menu to={'/product'} title={'Products'}/>
+      </div>
+    </div>
+  );
+};
+
+const Menu = ({ to, title }) => {
+  return (
+     <NavLink className={({isActive}) => isActive ? 'text-[tomato]' : ''} to={to} >{title}</NavLink> 
+  )
+};
+
+export default Navbar;
